7ed17dffb4e31652b25622cd9f1464445cdb9ee8
amit <amit@finin.in>
2021-01-18 13:15:50 +0530

    Refactored

--- furniture/tests/unit/test_services.py
+++ furniture/tests/unit/test_services.py
@@ -1,20 +1,16 @@
 import pytest
+from typing import List, Set
+from datetime import date, timedelta
+
 from allocation.domain import model
 from allocation.adapters import repository
 from allocation.service_layer import services
-from typing import List, Set
-from datetime import date, timedelta
+from allocation.service_layer import unit_of_work
 
 
 class FakeRepository(repository.AbstractRepository):
     def __init__(self, batches: List[model.Batch]):
         self._batches: Set[model.Batch] = set(batches)
-
-    @staticmethod
-    def for_batch(ref, sku, qty, eta=None):
-        return FakeRepository([
-            model.Batch(ref, sku, qty, eta),
-        ])
 
     def add(self, batch: model.Batch):
         self._batches.add(batch)
@@ -25,53 +21,58 @@ class FakeRepository(repository.AbstractRepository):
     def list(self) -> List[model.Batch]:
         return list(self._batches)
 
-
-class FakeSession():
-    committed = False
-
-    def commit(self):
-        self.committed = True
-
-
-def test_returns_allocations():
-    repo = FakeRepository.for_batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession())
-    assert result == "b1"
-
-
-def test_error_for_invalid_sku():
-    repo = FakeRepository.for_batch("b1", "AREALSKU", 100, eta=None)
-    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
-
-
-def test_commits():
-    repo = FakeRepository.for_batch("b1", "OMINOUS-MIRROR", 100, eta=None)
-    session = FakeSession()
-
-    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
-    assert session.committed is True
-
-
-def test_add_batch():
-    repo, session = FakeRepository([]), FakeSession()
-    services.add_batch("b1", "CRUNCHY-ARCHAIR", 100, None, repo, session)
-    assert repo.get("b1") is not None
-    assert session.committed
-
-
 today = date.today()
 tomrrow = today + timedelta(days=1)
 later = today + timedelta(days=2)
 
 
-def test_prefers_warehouse_batches_to_shipments():
-    in_stock_batch = model.Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
-    shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomrrow)
-    repo = FakeRepository([in_stock_batch, shipment_batch])
-    session = FakeSession()
+class FakeUnitOfWork(unit_of_work.AbstractUnitOfWork):
 
-    services.allocate("oref", "RETRO-CLOCK", 10, repo, session)
+    def __init__(self):
+        self.batches = FakeRepository([])
+        self.committed = False
+
+    def commit(self):
+        self.committed = True
+
+    def rollback(self):
+        pass
+
+
+def test_prefers_warehouse_batches_to_shipments():
+    uow = FakeUnitOfWork()
+    services.add_batch("in-stock-batch", "RETRO-CLOCK", 100, None, uow)
+    services.add_batch("shipment-batch", "RETRO-CLOCK", 100, tomrrow, uow)
+
+    in_stock_batch = uow.batches.get("in-stock-batch")
+    shipment_batch = uow.batches.get("shipment-batch")
+    services.allocate("oref", "RETRO-CLOCK", 10, uow)
 
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
+
+
+def test_add_batch():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "CRUNCHY=ARMCHAIR", 100, None, uow)
+    assert uow.batches.get("b1") is not None
+    assert uow.committed
+
+def test_allocate_returns_allocation():
+    uow = FakeUnitOfWork()
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, uow)
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, uow)
+    assert result == "batch1"
+
+
+def test_error_for_invalid_sku():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "AREALSKU", 100, None, uow)
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
+        services.allocate("o1", "NONEXISTENTSKU", 10, uow)
+
+def test_commits():
+    uow = FakeUnitOfWork()
+    services.add_batch("b1", "OMINOUS-MIRROR", 100, None, uow)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, uow)
+    assert uow.committed is True
